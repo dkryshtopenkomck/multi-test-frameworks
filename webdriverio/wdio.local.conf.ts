9ce1ce3a273e3e821b7777dc95da9f4f773bf781
Dmytro Kryshtopenko <dmytro.kryshtopenko@capgemini.com>
2025-02-14 20:14:21 +0900

    fix pp

--- webdriverio/wdio.local.conf.ts
+++ webdriverio/wdio.local.conf.ts
@@ -2,18 +2,30 @@ import { config as sharedConfig } from './wdio.shared.conf'
 
 export const config: WebdriverIO.Config = {
     ...sharedConfig,
-    ...{
-        capabilities: [{
-            browserName: 'chromium',
+    capabilities: [
+        {
+            browserName: 'chrome',
             'goog:chromeOptions': {
                 args: [
-                    '--no-sandbox',
-                    '--headless',
-                    '--disable-gpu',
-                    'window-size=1920,1080',
-                    '--disable-dev-shm-usage',
+                    '--headless',             // Run Chrome in headless mode
+                    'window-size=1920,1080',  // Set window size for headless mode
+                    '--no-sandbox',           // Ensure no sandboxing for Docker
+                    '--disable-extensions',   // Disable extensions
                 ],
             }
-        }]
-    }
+        },
+        {
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                args: [
+                    '--headless',            // Run Firefox in headless mode
+                    '--no-sandbox',          // Ensure no sandboxing for Docker
+                    '--disable-extensions',  // Disable extensions
+                    '--disable-plugins',     // Disable plugins
+                    'window-size=1920,1080', // Set window size for headless mode (Firefox)
+                    '--private',             // Start Firefox in private browsing mode
+                ],
+            }
+        }
+    ]
 }
